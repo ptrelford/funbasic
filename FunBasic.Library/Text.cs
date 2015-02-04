5c92ed0c32ade12c0ddc88f3e56702fe3536e166
Phillip Trelford <ptrelford@hotmail.com>
2015-02-04 18:00:11 +0000

    Added Text module with Append, GetLenth & GetSubText functions

---
+++ FunBasic.Library/Text.cs
@@ -0,0 +1,21 @@
+﻿
+namespace FunBasic.Library
+{
+   public static class Text
+   {
+      public static string Append(string a, string b)
+      {
+         return a + b;
+      }
+
+      public static int GetLength(string text)
+      {
+         return text.Length;
+      }
+
+      public static string GetSubText(string text, int index, int length)
+      {
+         return text.Substring(index, length);
+      }
+   }
+}
