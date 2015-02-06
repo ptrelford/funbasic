907f97d1e8fb13153c83d514f803b00be96bdeb4
Phillip Trelford <ptrelford@hotmail.com>
2015-02-06 22:21:53 +0000

    Numeric key name mappings

--- FunBasic.Store/Graphics.vb
+++ FunBasic.Store/Graphics.vb
@@ -596,6 +596,18 @@ Public Class Graphics
 
     Public ReadOnly Property LastKey As String Implements Library.IGraphics.LastKey
         Get
+            Select Case MyLastKey
+                Case VirtualKey.Number0 : Return "D0"
+                Case VirtualKey.Number1 : Return "D1"
+                Case VirtualKey.Number2 : Return "D2"
+                Case VirtualKey.Number3 : Return "D3"
+                Case VirtualKey.Number4 : Return "D4"
+                Case VirtualKey.Number5 : Return "D5"
+                Case VirtualKey.Number6 : Return "D6"
+                Case VirtualKey.Number7 : Return "D7"
+                Case VirtualKey.Number8 : Return "D8"
+                Case VirtualKey.Number9 : Return "D9"
+            End Select
             Return [Enum].GetName(GetType(VirtualKey), MyLastKey)
         End Get
     End Property
