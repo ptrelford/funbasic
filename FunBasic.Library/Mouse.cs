8eddd02b6219b75377058b1a2007d4889690ff5b
Phillip Trelford <ptrelford@hotmail.com>
2015-02-08 18:51:43 +0000

    Mouse HideCursor/ShowCursor function stubs

--- FunBasic.Library/Mouse.cs
+++ FunBasic.Library/Mouse.cs
@@ -4,6 +4,14 @@
    {
       public static bool IsLeftButtonDown { get; set; }
       public static bool IsRightButtonDown { get; set; }
+      public static double MouseX { get; set; }
+      public static double MouseY { get; set; }
+      public static void HideCursor()
+      {
+      }
+      public static void ShowCursor()
+      {
+      }
    }
 }
  
