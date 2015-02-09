c1cd512e1e26b6d089562e91a7766e826391d6c9
Phillip Trelford <ptrelford@hotmail.com>
2015-02-09 23:14:24 +0000

    Added FunBasic.App with 10 sample programs

---
+++ FunBasic.App/GroupedItemsPage.xaml.vb
@@ -0,0 +1,102 @@
+﻿' The Grouped Items Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234231
+
+''' <summary>
+''' A page that displays a grouped collection of items.
+''' </summary>
+Public NotInheritable Class GroupedItemsPage
+    Inherits Page
+
+    ''' <summary>
+    ''' NavigationHelper is used on each page to aid in navigation and 
+    ''' process lifetime management
+    ''' </summary>
+    Public ReadOnly Property NavigationHelper As Common.NavigationHelper
+        Get
+            Return Me._navigationHelper
+        End Get
+    End Property
+    Private _navigationHelper As Common.NavigationHelper
+
+    ''' <summary>
+    ''' This can be changed to a strongly typed view model.
+    ''' </summary>
+    Public ReadOnly Property DefaultViewModel As Common.ObservableDictionary
+        Get
+            Return Me._defaultViewModel
+        End Get
+    End Property
+    Private _defaultViewModel As New Common.ObservableDictionary()
+
+
+    Public Sub New()
+        InitializeComponent()
+        Me._navigationHelper = New Common.NavigationHelper(Me)
+        AddHandler Me._navigationHelper.LoadState,
+            AddressOf NavigationHelper_LoadState
+    End Sub
+
+    ''' <summary>
+    ''' Populates the page with content passed during navigation.  Any saved state is also
+    ''' provided when recreating a page from a prior session.
+    ''' </summary>
+    ''' <param name="sender">
+    ''' The source of the event; typically <see cref="NavigationHelper"/>
+    ''' </param>
+    ''' <param name="e">Event data that provides both the navigation parameter passed to
+    ''' <see cref="Frame.Navigate"/> when this page was initially requested and
+    ''' a dictionary of state preserved by this page during an earlier
+    ''' session.  The state will be null the first time a page is visited.</param>
+    Private Async Sub NavigationHelper_LoadState(sender As Object, e As Common.LoadStateEventArgs)
+        ' TODO: Create an appropriate data model for your problem domain to replace the sample data
+        Dim sampleDataGroups As IEnumerable(Of Data.SampleDataGroup) = Await Data.SampleDataSource.GetGroupsAsync()
+        Me.DefaultViewModel("Groups") = sampleDataGroups
+    End Sub
+    ''' <summary>
+    ''' Invoked when a group header is clicked.
+    ''' </summary>
+    ''' <param name="sender">The Button used as a group header for the selected group.</param>
+    ''' <param name="e">Event data that describes how the click was initiated.</param>
+    Private Sub Header_Click(sender As Object, e As RoutedEventArgs)
+
+        ' Determine what group the Button instance represents
+        Dim group As Object = DirectCast(sender, FrameworkElement).DataContext
+
+        ' Navigate to the appropriate destination page, configuring the new page
+        ' by passing required information as a navigation parameter
+        Me.Frame.Navigate(GetType(GroupDetailPage), DirectCast(group, Data.SampleDataGroup).UniqueId)
+    End Sub
+
+    ''' <summary>
+    ''' Invoked when an item within a group is clicked.
+    ''' </summary>
+    ''' <param name="sender">The GridView (or ListView when the application is snapped)
+    ''' displaying the item clicked.</param>
+    ''' <param name="e">Event data that describes the item clicked.</param>
+    Private Sub ItemView_ItemClick(sender As Object, e As ItemClickEventArgs)
+
+        ' Navigate to the appropriate destination page, configuring the new page
+        ' by passing required information as a navigation parameter
+        Dim itemId As String = DirectCast(e.ClickedItem, Data.SampleDataItem).UniqueId
+        Me.Frame.Navigate(GetType(ItemDetailPage), itemId)
+    End Sub
+
+#Region "NavigationHelper registration"
+
+    ''' The methods provided in this section are simply used to allow
+    ''' NavigationHelper to respond to the page's navigation methods.
+    ''' 
+    ''' Page specific logic should be placed in event handlers for the  
+    ''' <see cref="Common.NavigationHelper.LoadState"/>
+    ''' and <see cref="Common.NavigationHelper.SaveState"/>.
+    ''' The navigation parameter is available in the LoadState method 
+    ''' in addition to page state preserved during an earlier session.
+
+    Protected Overrides Sub OnNavigatedTo(e As NavigationEventArgs)
+        _navigationHelper.OnNavigatedTo(e)
+    End Sub
+
+    Protected Overrides Sub OnNavigatedFrom(e As NavigationEventArgs)
+        _navigationHelper.OnNavigatedFrom(e)
+    End Sub
+#End Region
+End Class
---
+++ FunBasic.App/ItemDetailPage.xaml.vb
@@ -0,0 +1,164 @@
+﻿Imports System.Text, System.Reflection, System.Threading
+Imports FunBasic.Interpreter, FunBasic.Library, FunBasic.Store
+Imports Windows.UI
+
+' The Item Detail Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234232
+
+''' <summary>
+''' A page that displays details for a single item within a group while allowing
+''' gestures to flip through other items belonging to the same group.
+''' </summary>
+Public NotInheritable Class ItemDetailPage
+    Inherits Page
+
+    ''' <summary>
+    ''' NavigationHelper is used on each page to aid in navigation and 
+    ''' process lifetime management
+    ''' </summary>
+    Public ReadOnly Property NavigationHelper As Common.NavigationHelper
+        Get
+            Return Me._navigationHelper
+        End Get
+    End Property
+    Private _navigationHelper As Common.NavigationHelper
+
+    ''' <summary>
+    ''' This can be changed to a strongly typed view model.
+    ''' </summary>
+    Public ReadOnly Property DefaultViewModel As Common.ObservableDictionary
+        Get
+            Return Me._defaultViewModel
+        End Get
+    End Property
+    Private _defaultViewModel As New Common.ObservableDictionary()
+
+
+    Public Sub New()
+        InitializeComponent()
+        Me._navigationHelper = New Common.NavigationHelper(Me)
+        AddHandler Me._navigationHelper.LoadState,
+            AddressOf NavigationHelper_LoadState
+    End Sub
+
+    ''' <summary>
+    ''' Populates the page with content passed during navigation.  Any saved state is also
+    ''' provided when recreating a page from a prior session.
+    ''' </summary>
+    ''' <param name="sender">
+    ''' The source of the event; typically <see cref="NavigationHelper"/>
+    ''' </param>
+    ''' <param name="e">Event data that provides both the navigation parameter passed to
+    ''' <see cref="Frame.Navigate"/> when this page was initially requested and
+    ''' a dictionary of state preserved by this page during an earlier
+    ''' session.  The state will be null the first time a page is visited.</param>
+    Private Async Sub NavigationHelper_LoadState(sender As Object, e As Common.LoadStateEventArgs)
+        ' TODO: Create an appropriate data model for your problem domain to replace the sample data
+        Dim item As Data.SampleDataItem = Await Data.SampleDataSource.GetItemAsync(DirectCast(e.NavigationParameter, String))
+        Me.DefaultViewModel("Item") = item
+
+        Dim dataUri As New Uri(String.Format("ms-appx:///Programs/{0}", item.Program))
+
+        Dim file As StorageFile = Await StorageFile.GetFileFromApplicationUriAsync(dataUri)
+        Dim jsonText As String = Await FileIO.ReadTextAsync(file)
+        Me.Code.Text = jsonText
+    End Sub
+
+#Region "NavigationHelper registration"
+
+    ''' The methods provided in this section are simply used to allow
+    ''' NavigationHelper to respond to the page's navigation methods.
+    ''' 
+    ''' Page specific logic should be placed in event handlers for the  
+    ''' <see cref="Common.NavigationHelper.LoadState"/>
+    ''' and <see cref="Common.NavigationHelper.SaveState"/>.
+    ''' The navigation parameter is available in the LoadState method 
+    ''' in addition to page state preserved during an earlier session.
+
+    Protected Overrides Sub OnNavigatedTo(e As NavigationEventArgs)
+        _navigationHelper.OnNavigatedTo(e)
+    End Sub
+
+    Protected Overrides Sub OnNavigatedFrom(e As NavigationEventArgs)
+        _navigationHelper.OnNavigatedFrom(e)
+    End Sub
+#End Region
+
+    Dim ffi As New FFI()
+    Dim timer As New FunBasic.Store.Timer()
+    Dim cancelToken As New CancelToken()
+    Dim done As ManualResetEvent = New ManualResetEvent(False)
+
+    Private Async Sub StartButton_Click(sender As Object, e As RoutedEventArgs) _
+        Handles StartButton.Click
+        StartButton.IsEnabled = False
+        StopButton.IsEnabled = True
+        Code.IsEnabled = False
+
+        cancelToken = New CancelToken()
+
+        Dim program = Code.Text
+        Await Task.Run(Sub() Start(program))
+    End Sub
+
+    Private Async Sub StopButton_Click(sender As Object, e As RoutedEventArgs) _
+        Handles StopButton.Click
+        StopButton.IsEnabled = False
+
+        ffi.Unhook()
+        timer.Pause()
+        cancelToken.Cancel()
+
+        Await Task.Run(Sub() [Stop]())
+    End Sub
+
+    Private Sub InitLibrary()
+        Dim c = New Console(Me.MyConsole)
+        TextWindow.Console = c
+        Dim theTurtle = Me.MyTurtle
+        Me.MyGraphics.Children.Clear()
+        Me.MyGraphics.Background = New SolidColorBrush(Colors.White)
+        Me.MyShapes.Children.Clear()
+        Me.MyShapes.Children.Add(theTurtle)
+        Dim graphics = New Graphics(Me.MyGraphics, Me.MyShapes, Me.MyTurtle)
+        GraphicsWindow.Graphics = graphics
+        Turtle.Graphics = graphics
+        timer.Interval = -1
+        FunBasic.Library.Timer.SetTimer(timer)
+    End Sub
+
+    Private Async Sub Start(program As String)
+        Await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, _
+                            Sub() InitLibrary())
+
+        Try
+            Runtime.Run(program, ffi, cancelToken)
+        Catch ex As Exception
+            System.Diagnostics.Debug.WriteLine(ex)
+            TextWindow.Console.WriteLine(ex.Message)
+        Finally
+            done.Set()
+        End Try
+        If Not ffi.IsHooked Then
+            timer.Pause()
+            Await EnableStartButton()
+        End If
+    End Sub
+
+    Private Async Sub [Stop]()
+        If done IsNot Nothing Then
+            Dim success = done.WaitOne()
+            done.Reset()
+        End If
+        Await EnableStartButton()
+    End Sub
+
+    Private Async Function EnableStartButton() As Task
+        Await Dispatcher.RunAsync(Core.CoreDispatcherPriority.Normal,
+                    Sub()
+                        StartButton.IsEnabled = True
+                        StopButton.IsEnabled = False
+                        Me.Code.IsEnabled = True
+                    End Sub)
+    End Function
+
+End Class
